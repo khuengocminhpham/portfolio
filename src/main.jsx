285976336e213eaea6154462a4faa6684b0ce0c7
khuengocminhpham <pham178@purdue.edu>
2025-06-23 12:34:32 +0700

    remove app component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Project from "./pages/Project.jsx";
+import Contact from "./pages/Contact.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
